92d6bb2e43cb5ce1967e269d18a2dfcb456ba850
santipm29 <santipmartinez@outlook.com>
2019-06-10 16:01:25 -0500

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# SEND GRID - PYTHON
+
+## Create virtualenv
+```bash
+virtualenv venv
+```
+## Activate virtualenv
+```bash
+.\venv\Scripts\activate.ps1
+```
+or
+```bash
+.\venv\Scripts\activate.bat
+```
+
+## Installation
+```bash
+pip install -r requirements.txt
+```
+
+## Test
+```bash
+py .\main.py
+```
---
+++ main.py
@@ -0,0 +1,16 @@
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+message = Mail(
+    from_email='from_email@example.com',
+    to_emails='to_emails@example.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient("API_KEY")
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(str(e))
